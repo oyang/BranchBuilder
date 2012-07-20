91b7fc9f12a7ea8f6a9f2bd42c7a42a29b9ae0ca
oliver yang <oyang@sugarcrm.com>
2012-07-20 10:58:32 -0700

    Update tables

--- testing.bash
+++ testing.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+echo "show schemas"
+sqlite3 branchBuilder '.schema'
+echo 
+echo 
 echo "Get all from builds_status"
 sqlite3 branchBuilder 'select * from builds_status'
-echo '======================'
 echo 
 echo '======================'
 echo "Get all from builds"
@@ -11,4 +13,8 @@ echo
 echo 
 echo "Get all status from builds"
 sqlite3 branchBuilder 'select task_id, status from builds'
+echo 
+echo 
+echo "Get all from build_configs"
+sqlite3 branchBuilder 'select * from build_configs'
 
